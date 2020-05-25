738ddbf791b259fdaae1cd855cee2d42cb35aa07
Валерия Мухаметшина <muhsmeyshina.lera@gmail.com>
2020-05-25 05:21:01 +0500

    Start

---
+++ 56jtyj/56jtyj/View/LeraBD.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data.Entity;
+using Microsoft.Win32;
+using System.IO;
+using _56jtyj.ModelDb;
+
+namespace _56jtyj.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LeraBD.xaml
+    /// </summary>
+    public partial class LeraBD : Window
+    {
+        public LeraBD()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MaslovEntities Lera = new MaslovEntities();
+            Lera.Sheet1_.Load();
+            OpenBD.ItemsSource = Lera.Sheet1_.Local.ToBindingList();
+            BD.Visibility = Visibility.Hidden;
+
+        }
+    }
+}
---
+++ 56jtyj/56jtyj/View/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _56jtyj.ModelDb;
+
+
+namespace _56jtyj
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User user;
+        MaslovEntities Lera;
+        public MainWindow()
+        {
+            Lera = new MaslovEntities();
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = Loginn.Text;
+            string password = Passwordd.Text;
+
+            MaslovEntities Lera = new MaslovEntities();
+            Lera.User.Load();
+
+            var user = Lera.User
+                .Where(u => u.Login == login && u.Password == password)
+                .FirstOrDefault();
+            if (user == null)
+                MessageBox.Show("Неверно");
+            if (user != null)
+            {
+                View.LeraBD window1 = new View.LeraBD();
+                window1.Show();
+            }
+            
+
+              
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            user = new User();
+            user.Name = Name.Text;
+            user.LastName = LastName.Text;
+            user.Age = Convert.ToInt32(Age.Text);
+            user.Login = LoginRegistr.Text;
+            user.Password = PasswordRegistr.Text;
+            Lera.User.Add(user);
+            MessageBox.Show("Успешно зарегистрировались");
+            Lera.SaveChanges();
+        }
+    }
+}
